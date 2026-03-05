bfc256c67ad4cfbfaeee18df3205f0b2d4eed3f9
TheFlyyingPigs <theflyingpigs10@gmail.com>
2026-03-04 19:31:38 -0600

    Revert "Implemented load_scene()"
    This reverts commit 16a74858d3ff1047b11a689fca6792cffdceebdb.

--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -1,9 +1,5 @@
 extends Node
 
-@onready var world := get_node("/root/world/")
-
-var loaded_scene : PackedScene
-
 # ADD ACTUAL RESOURCE TYPES!
 enum ItemType {
 	WATER,
@@ -19,9 +15,4 @@ func add_item(type : ItemType):
 
 func _ready() -> void:
 	randomize()
-	
-
-func load_scene(scene):
-	loaded_scene = scene
-	world.add_child(loaded_scene.instantiate())
 	
--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,7 +1,5 @@
 extends Node3D
 
-@onready var outside = load("res://Scenes/outside_world.tscn")
-
 func _ready() -> void:
 	Gui.show_screen(Gui.ScreenType.MAIN_MENU)
-	Globals.load_scene(outside)
+	
