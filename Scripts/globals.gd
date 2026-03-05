16a74858d3ff1047b11a689fca6792cffdceebdb
TheFlyyingPigs <theflyingpigs10@gmail.com>
2026-03-04 18:24:22 -0600

    Implemented load_scene()
    -new func in globals script that loads a new scene to the world

--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -1,5 +1,9 @@
 extends Node
 
+@onready var world := get_node("/root/world/")
+
+var loaded_scene : PackedScene
+
 # ADD ACTUAL RESOURCE TYPES!
 enum ItemType {
 	WATER,
@@ -15,4 +19,9 @@ func add_item(type : ItemType):
 
 func _ready() -> void:
 	randomize()
+	
+
+func load_scene(scene):
+	loaded_scene = scene
+	world.add_child(loaded_scene.instantiate())
 	
--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@onready var outside = load("res://Scenes/outside_world.tscn")
+
 func _ready() -> void:
 	Gui.show_screen(Gui.ScreenType.MAIN_MENU)
-	
+	Globals.load_scene(outside)
